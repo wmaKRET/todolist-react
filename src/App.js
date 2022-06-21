290989bec0411c816aa44b6f7648e2d2a8105bb0
wmaKRET <wkret91.2@gmail.com>
2022-06-21 11:40:06 +0200

    js functions developed (to show aleart messages), addTask function fixed

--- src/App.js
+++ src/App.js
@@ -4,22 +4,7 @@ import Menu from "./Menu"
 import List from "./List"
 
 function App() {
-  const [tasks, setTasks] = React.useState(testTasks())
-
-  function testTasks(){
-    return [
-      {
-        id: Date.now().toString() + 1,
-        value: 'task 1',
-        isCompleted: false
-      },
-      {
-        id: Date.now().toString() + 2,
-        value: 'task 2',
-        isCompleted: true
-      }
-    ]
-  }
+  const [tasks, setTasks] = React.useState([])
 
   function createTask(inputValue){
     return {
@@ -31,12 +16,15 @@ function App() {
 
   function displayMessage(message, action){
     const alertMessage = document.getElementById('title-oralertmessage')
+    const inputText = document.getElementById('taskInput')
     alertMessage.textContent = message
     alertMessage.classList.add(action)
+    inputText.disabled = true
     setTimeout(() => {
       alertMessage.textContent = 'To-do list'
       alertMessage.classList.remove(action)
-    }, 1500)
+      inputText.disabled = false
+    }, 1000)
   }
 
   function addTask(){
@@ -47,15 +35,25 @@ function App() {
     }
     setTasks(prevTasks => [...prevTasks, createTask(inputText.value)])
     displayMessage('Task added!', 'action-success')
-    inputText.value = ''
+    setTimeout(() => inputText.value = '', 0)
   }
 
   function clearCompleted(){
-    console.log('clear')
+    if (!tasks.length) {
+      displayMessage('There are no completed tasks.', 'action-failure')
+      return
+    }
+    setTasks(prevTasks => prevTasks.filter(task => !task.isCompleted))
+    displayMessage('Completed tasks has been deleted.', 'action-success')
   }
 
   function deleteAll(){
+    if (!tasks.length) {
+      displayMessage('There are no tasks to delete.', 'action-failure')
+      return
+    }
     setTasks([])
+    displayMessage('All tasks has been deleted.', 'action-success')
   }
 
   function deleteTask(id){
